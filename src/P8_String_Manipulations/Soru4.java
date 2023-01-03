f613f7e70872ae99bff8149a687b876ad592fd9d
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-03 15:00:59 +0300

    3.1.23

---
+++ src/P8_String_Manipulations/Soru4.java
@@ -0,0 +1,13 @@
+package P8_String_Manipulations;
+
+import java.util.Scanner;
+
+public class Soru4 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen şifrenizi giriniz: ");
+        String sifre=scan.nextLine();
+
+       // if (sifre.substring(0,1).||sifre.substring(0,1)<=122)
+    }
+}
