be2786f7ccae8ccdf6571c779620b1ec707782ef
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-14 09:53:42 +0300

    14.01.23

--- src/P9_ForLoop/Soru1.java
+++ src/P9_ForLoop/Soru1.java
@@ -1,4 +1,9 @@
 package P9_ForLoop;
 
 public class Soru1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i+" ");
+        }
+    }
 }
---
+++ src/P9_ForLoop/Soru2.java
@@ -0,0 +1,16 @@
+package P9_ForLoop;
+
+import java.util.Scanner;
+
+public class Soru2 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Bir sayi giriniz: ");
+        int girilenSayi= scan.nextInt();
+        for (int i = 1; i <=girilenSayi ; i++) {
+            if (i%7==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
