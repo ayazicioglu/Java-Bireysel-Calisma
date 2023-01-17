9adccc3813c808204d41f0c0ca67f425efb257b0
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-17 23:53:51 +0300

    17.01.23

---
+++ src/P12_DoWhileLoop/Soru1.java
@@ -0,0 +1,12 @@
+package P12_DoWhileLoop;
+
+public class Soru1 {
+    public static void main(String[] args) {
+        char ilkHarf='k';
+        do {
+            System.out.print(ilkHarf+",");
+            ilkHarf++;
+
+        }while (ilkHarf<='t');
+    }
+}
