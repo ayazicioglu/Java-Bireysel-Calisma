d6ffb9a02a35291a0b853a5037b6dbfcb27465d4
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-17 21:59:34 +0300

    17.01.23

--- src/P11_WhileLoop/Soru2.java
+++ src/P11_WhileLoop/Soru2.java
@@ -1,7 +1,18 @@
 package P11_WhileLoop;
 
+import java.util.Scanner;
+
 public class Soru2 {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen bir sayı giriniz");
+        int sayi= scan.nextInt();
+        int rakamlarToplami=0;
+        while (sayi>0) {
+            rakamlarToplami += sayi % 10;
+            sayi = sayi / 10;
 
+        }
+        System.out.println("rakamlar toplami: "+rakamlarToplami);
     }
 }
